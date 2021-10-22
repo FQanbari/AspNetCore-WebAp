2f950b3dd9217682d40af2a6a6c5a981e63917c9
FQanbari <fatemeh.ganbari1993@gmail.com>
2021-10-22 11:38:19 +0330

    Data Layer

--- AspNetCore-WebApi.Common/Utilities/ModelBuilderExtensions.cs
+++ AspNetCore-WebApi.Common/Utilities/ModelBuilderExtensions.cs
@@ -11,61 +11,7 @@ namespace AspNetCore_WebApi.Common.Utilities
 {
     public static class ModelBuilderExtensions
     {
-        /// <summary>
-        /// Singularizin table name like Posts to Post or People to Person
-        /// </summary>
-        /// <param name="modelBuilder"></param>
-        public static void AddSingularizingTableNameConvention(this ModelBuilder modelBuilder)
-        {
-            Pluralizer pluralizer = new Pluralizer();
-            foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
-            {
-                string tableName = entityType.Relational().TableName;
-                entityType.Relational().TableName = pluralizer.Singularize(tableName);
-            }
-        }
-
-        /// <summary>
-        /// Pluralizing table name like Post to Posts or Person to People
-        /// </summary>
-        /// <param name="modelBuilder"></param>
-        public static void AddPluralizingTableNameConvention(this ModelBuilder modelBuilder)
-        {
-            Pluralizer pluralizer = new Pluralizer();
-            foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
-            {
-                string tableName = entityType.Relational().TableName;
-                entityType.Relational().TableName = pluralizer.Pluralize(tableName);
-            }
-        }
-
-        /// <summary>
-        /// Set NEWSEQUENTIALID() sql function for all columns named "Id"
-        /// </summary>
-        /// <param name="modelBuilder"></param>
-        /// <param name="mustBeIdentity">Set to true if you want only "Identity" guid fields that named "Id"</param>
-        public static void AddSequentialGuidForIdConvention(this ModelBuilder modelBuilder)
-        {
-            modelBuilder.AddDefaultValueSqlConvention("Id", typeof(Guid), "NEWSEQUENTIALID()");
-        }
-
-        /// <summary>
-        /// Set DefaultValueSql for sepecific property name and type
-        /// </summary>
-        /// <param name="modelBuilder"></param>
-        /// <param name="propertyName">Name of property wants to set DefaultValueSql for</param>
-        /// <param name="propertyType">Type of property wants to set DefaultValueSql for </param>
-        /// <param name="defaultValueSql">DefaultValueSql like "NEWSEQUENTIALID()"</param>
-        public static void AddDefaultValueSqlConvention(this ModelBuilder modelBuilder, string propertyName, Type propertyType, string defaultValueSql)
-        {
-            foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
-            {
-                IMutableProperty property = entityType.GetProperties().SingleOrDefault(p => p.Name.Equals(propertyName, StringComparison.OrdinalIgnoreCase));
-                if (property != null && property.ClrType == propertyType)
-                    property.Relational().DefaultValueSql = defaultValueSql;
-            }
-        }
-
+        
         /// <summary>
         /// Set DeleteBehavior.Restrict by default for relations
         /// </summary>
