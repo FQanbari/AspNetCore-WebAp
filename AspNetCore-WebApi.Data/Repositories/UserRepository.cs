9b0b128b64264675158e4f4d22d2812125477c59
FQanbari <fatemeh.ganbari1993@gmail.com>
2021-10-22 12:58:39 +0330

    Repository

---
+++ AspNetCore-WebApi.Data/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using AspNetCore_WebApi.Common;
+using AspNetCore_WebApi.Common.Utilities;
+using AspNetCore_WebApi.Data.Contracts;
+using AspNetCore_WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetCore_WebApi.Data.Repositories
+{
+    public class UserRepository : Repository<User>
+    {
+        public UserRepository(ApplicationDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+        public Task<User> GetByUserAndPass(string username, string password, CancellationToken cancellationToken)
+        {
+            var passwordHash = SecurityHelper.GetSha256Hash(password);
+            return Table.Where(p => p.UserName == username && p.PasswordHash == passwordHash).SingleOrDefaultAsync(cancellationToken);
+        }
+
+    }
+}
